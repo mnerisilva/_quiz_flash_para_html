e1c476ea6431c108ca25fbe58df64184a43116d1
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-06-22 22:14:24 -0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const botao_proxima = document.querySelector(".botao-proxima");
 const mensagem_abertura = document.querySelector('.mensagem-abertura');
+const questao1 = document.querySelector('.questao-1');
+const botao = document.querySelector('.botao');
 
 botao_proxima.addEventListener('click', function(){
     mensagem_abertura.classList.add('esconde');
     setInterval(function(){
-        mensagem_abertura.classList.add("oculta");
+        mensagem_abertura.classList.add("retira");
+        questao1.classList.remove('retira');
+        botao.textContent = 'enviar';
+    },1500);
+    setInterval(function(){
+        questao1.classList.remove("esconde");
     },1500);
 });
