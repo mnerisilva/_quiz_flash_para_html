2ff19e2e926d93ae3bd31f8516a9b286a5c1c373
mnerisilva <marceloneri@gmail.com>
2021-06-23 17:06:02 -0300

    update

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ const mensagem_abertura = document.querySelector('.mensagem-abertura');
 const questao1 = document.querySelector('.questao-1');
 const botao = document.querySelector('.botao');
 
-botao_proxima.addEventListener('click', function(){
-    mensagem_abertura.classList.add('esconde');
-    setInterval(function(){
-        mensagem_abertura.classList.add("retira");
-        questao1.classList.remove('retira');
-        botao.textContent = 'enviar';
-    },1500);
-    setInterval(function(){
-        questao1.classList.remove("esconde");
-    },1500);
+let tela = 1;
+
+console.log(tela);
+
+botao_proxima.addEventListener('click', function(event){
+    tela += 1;
+    console.log(event.target.name)
+    console.log(tela);
+    if (tela == 2){
+        
+    }else{
+        mensagem_abertura.classList.add('esconde');
+        setInterval(function(){
+            mensagem_abertura.classList.add("retira");
+            questao1.classList.remove('retira');
+            botao.textContent = 'enviar';
+        },1500);
+        setInterval(function(){
+            questao1.classList.remove("esconde");
+        },1500);
+    }
 });
