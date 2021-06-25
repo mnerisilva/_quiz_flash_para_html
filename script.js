9f160c547d9f81b0c9c9fbe4bad1600cff33880c
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-06-25 19:32:51 -0300

    update

--- script.js
+++ script.js
@@ -2,18 +2,30 @@ const botao_proxima = document.querySelector(".botao-proxima");
 const mensagem_abertura = document.querySelector('.mensagem-abertura');
 const questao1 = document.querySelector('.questao-1');
 const botao = document.querySelector('.botao');
+const op1 = document.querySelector('.op1');
+const op2 = document.querySelector('.op2');
+const op3 = document.querySelector('.op3');
+const op4 = document.querySelector('.op4');
+const op5 = document.querySelector('.op5');
+const op6 = document.querySelector('.op6');
+
+const gabarito1 = op1.querySelector('.select select').dataset.gabarito;
+const gabarito2 = op2.querySelector('.select select').dataset.gabarito;
+const gabarito3 = op3.querySelector('.select select').dataset.gabarito;
+const gabarito4 = op4.querySelector('.select select').dataset.gabarito;
+const gabarito5 = op5.querySelector('.select select').dataset.gabarito;
+const gabarito6 = op6.querySelector('.select select').dataset.gabarito;
+
+
+const container_popup_mensagens = document.querySelector('.container-popup-mensagens');
+const janela_popup = document.querySelector('.janela-popup');
 
 let tela = 1;
+let atividade;
 
-console.log(tela);
-
+let statusPreenchimento;
 botao_proxima.addEventListener('click', function(event){
-    tela = tela + 1;
-    console.log(event.target.name)
-    console.log(tela);
-    if (tela < 2){
-        console.log('tela menos que 2');
-    }else{
+    if (tela == 1){
         mensagem_abertura.classList.add('esconde');
         setInterval(function(){
             mensagem_abertura.classList.add("retira");
@@ -23,5 +35,38 @@ botao_proxima.addEventListener('click', function(event){
         setInterval(function(){
             questao1.classList.remove("esconde");
         },1500);
+        tela = tela + 1;
+    }else if(tela == 2) {
+        atividade = 1;
+        if(!verificaSeFoiPreenchido(atividade)){
+            container_popup_mensagens.classList.remove('retira');
+            janela_popup.innerHTML = '<h3>Você precisa responder à questão!!!</h3';
+            setTimeout(function(){
+                container_popup_mensagens.classList.remove('esconde');
+            },.100);
+        }
+        console.log('Status do preenchimento: '+verificaSeFoiPreenchido(atividade));
     }
 });
+
+
+function verificaSeFoiPreenchido(_numQuestao){
+    const valoresEscolhidos = [];
+    //console.log(_numQuestao);
+    const nodeListPreenchidos = document.querySelectorAll('.questao-1 .opcoes .select select')
+    let numeroDeOpcoes = nodeListPreenchidos.length;
+    let contador = 1;
+    for (var indiceDaOpcao of nodeListPreenchidos) {
+        //escolhidas[]
+        //console.log(indiceDaOpcao.value);
+        valoresEscolhidos[contador] = indiceDaOpcao.value;
+        contador = contador + 1;
+      }
+      contador = 1;
+      //console.log(valoresEscolhidos);
+      return !valoresEscolhidos.includes("0");
+}
+
+function retornaValorOpcoesEscolhidas(){
+    return true;
+}
